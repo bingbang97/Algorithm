1f6c78e0d3d61113c0a4c11680b840e70ccb9db9
BingBang <byoung1997s4@gmail.com>
2023-03-02 20:25:15 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 394.87 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,26 +1,17 @@
+from itertools import zip_longest as zip
+
+def tolist(l):
+    n=[]
+    for i,d in enumerate(l):
+        for _ in range(d):
+            n.append(i+1)
+    return n
+
 def solution(cap, n, deliveries, pickups):
-    answer = 0
-    while deliveries or pickups:
-        deliver_cap, pickup_cap = cap, cap
-        while deliveries and deliveries[-1] == 0:
-            deliveries.pop()
-        while pickups and pickups[-1] == 0:
-            pickups.pop()
-        answer += max(len(deliveries), len(pickups)) * 2
-
-        while deliveries and deliver_cap > 0:
-            box = deliveries.pop()
-            if box <= deliver_cap:
-                deliver_cap -= box
-            else:
-                deliveries.append(box - deliver_cap)
-                break
-        while pickups and pickup_cap > 0:
-            box = pickups.pop()
-            if box <= pickup_cap:
-                pickup_cap -= box
-            else:
-                pickups.append(box - pickup_cap)
-                break
-
-    return answer
+    d=tolist(deliveries)
+    p=tolist(pickups)
+    d.reverse()
+    p.reverse()
+    d=d[::cap]
+    p=p[::cap]
+    return 2*sum([max(x,y) for x,y in zip(d,p,fillvalue=0)])
